41e30da075cf6863071ef915a29cbd439b55a032
Diego Villabrille Seca <al375496@uji.es>
2020-11-19 00:18:00 +0100

    Added behaviour of in dialogue characters

---
+++ Assets/Scripts/CharacterDisplayHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterDisplayHelper : MonoBehaviour
+{
+    [SerializeField] private Sprite textBoxSprite;
+
+    [SerializeField] private Color greyOutColor;
+
+    [SerializeField] private Sprite[] emotionSprites;
+    public Sprite TextBoxSprite { get; private set; }
+    public Color GreyOutColor { get; set; }
+
+
+    private Image characterImage;
+    private Emotion activeEmotion;
+    private bool isTalking;
+
+    public Emotion ActiveEmotion
+    {
+        get { return activeEmotion; }
+        set //when the active emotion is set also change the sprite accordingly
+        {
+            activeEmotion = value;
+            Debug.Log (emotionSprites.Length);
+            characterImage.sprite = emotionSprites[(int)activeEmotion];
+        }
+    }
+
+    public bool IsTalking
+    {
+        get { return isTalking; }
+        set //when the character is not talking make it loog greyed out
+        {
+            if (value)
+               characterImage.color = Color.white;
+            else
+                characterImage.color = greyOutColor;
+
+        }
+    }
+
+    void Start()
+    {
+        characterImage = GetComponent<Image>();
+        ActiveEmotion = Emotion.Neutral;
+    }
+}
---
+++ Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+    private CharacterDisplayHelper lCharacter;
+    private CharacterDisplayHelper rCharacter;
+  //  private Image boxImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
