b8d28f7abb53d41c56c2cb5f7107af26652c7511
Diego Villabrille Seca <al375496@uji.es>
2020-11-29 13:01:14 +0100

    Updated Dialogue tooltip to make using it easier

--- Assets/Scripts/Dialogues/Dialogue.cs
+++ Assets/Scripts/Dialogues/Dialogue.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue : MonoBehaviour
 {
-    [Tooltip("Start the line with -lc for left character or -rc for right character.\nUse -ss to make the dialogue load a new scene and close the dialogue (you can use -nd in the next line if you need to before the dialogue closes).\nUse -sa to load a new scene in aditive mode and close the dialogue (you can use -nd in the next line if you need to before the dialogue closes).\nUse -nd to end this dialogue and set the next one in this gameObject active\nUse -rl to display a random line of dialogue from this dialogue and set the dialogue to be closed")]
+    [Tooltip("Start the line with -lc for left character or -rc for right character.\nUse -ss to make the dialogue load a new scene and close the dialogue (you can use -nd in the next line if you need to before the dialogue closes).\nUse -sa to load a new scene in aditive mode and close the dialogue (you can use -nd in the next line if you need to before the dialogue closes).\nUse -nd to end this dialogue and set the next one in this gameObject active.\n" +
+        "Use -rl to display a random line of dialogue from this dialogue and set the dialogue to be closed.\nUse [index]-eo to enable a GameObject from interactedGameObjects.\nUse [index]-do to disable a GameObject from interactedGameObjects.\nUse [index]-nd to set active next dialogue in a gameObject from interactedGameObjects.")]
     public string[] Lines;
 
     public CharacterDisplayHelper LCharacter = null;
