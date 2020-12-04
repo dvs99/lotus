4e09402734e7dae1bdb8d38f103d6f0a269d8c6f
unknown <al385783@uji.es>
2020-12-04 18:37:20 +0100

    Sprites ya no desaparecen

--- Assets/Scripts/Dialogues/DialogueManager.cs
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -26,7 +26,6 @@ public class DialogueManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
