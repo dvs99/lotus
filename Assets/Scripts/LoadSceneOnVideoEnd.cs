3fceec545b0305a379bb9396554d3d238c56419e
Diego Villabrille Seca <al375496@uji.es>
2020-12-15 23:42:56 +0100

    Cinemática añadida

---
+++ Assets/Scripts/LoadSceneOnVideoEnd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class LoadSceneOnVideoEnd : MonoBehaviour
+{
+    private VideoPlayer video;
+    public string NextScene;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+}
