315b684830cba472fd718393bc9be306f910235f
Luna Celeste Ferrer Escandell <al385717@uji.es>
2020-12-19 13:57:12 +0100

    Botón para salir del juego

---
+++ Assets/Scripts/Salir.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salir : MonoBehaviour
+{
+    public void Salida()
+    {
+        Application.Quit();
+    }
+   
+}
